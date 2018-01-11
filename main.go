7cf230f9215bd279340d88333d1509f5efb9e15a
Takashi Masuda <masutaka.net@gmail.com>
2018-01-12 00:56:21 +0900

    Add http server

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"errors"
+	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"os"
 	"strings"
 	"time"
@@ -22,7 +24,60 @@ func main() {
 		if err := cli(); err != nil {
 			log.Fatal(err)
 		}
+	} else {
+		if err := server(); err != nil {
+			log.Fatal(err)
+		}
 	}
+}
+
+func server() error {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return errors.New("$PORT must be set")
+	}
+
+	var httpServer http.Server
+
+	http.HandleFunc("/", handler)
+	log.Println("start http listening :" + port)
+	httpServer.Addr = ":" + port
+	log.Println(httpServer.ListenAndServe())
+
+	return nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+
+	feed, err := getFeed()
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, feed)
+}
+
+func getFeed() (string, error) {
+	c, err := redis.DialURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		return "", err
+	}
+	defer c.Close()
+
+	name := "feed:" + os.Getenv("QIITA_TEAM_NAME")
+
+	s, err := redis.String(c.Do("GET", name))
+	if err != nil {
+		return "", err
+	}
+
+	return s, nil
 }
 
 func cli() error {
@@ -172,12 +227,6 @@ func save(content []byte) error {
 	if _, err := c.Do("SET", name, content); err != nil {
 		return err
 	}
-
-	// s, err := redis.String(c.Do("GET", name))
-	// if err != nil {
-	// 	return err
-	// }
-	// os.Stdout.Write([]byte(s))
 
 	return nil
 }
