a4acd2c2c16362d76238a169f27b0daa5d37807c
Takashi Masuda <masutaka.net@gmail.com>
2018-01-23 00:18:02 +0900

    Refactor with cli.go

---
+++ cli.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/xml"
+	"fmt"
+	"html/template"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/ktsujichan/qiita-sdk-go/qiita"
+	"golang.org/x/tools/blog/atom"
+)
+
+// CLI is a qiita-team-feed CLI
+type CLI struct {
+	client *qiita.Client
+}
+
+// NewCLI returns a new CLI
+func NewCLI(teamName, accessToken string) (*CLI, error) {
+	config := qiita.NewConfig()
+	config.WithEndpoint(fmt.Sprintf("https://%s.qiita.com", teamName))
+
+	c, err := qiita.NewClient(accessToken, *config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CLI{client: c}, nil
+}
+
+// Run saves Qiita:Team feed to Redis
+func (c *CLI) Run(feedItemNum uint) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	qiitaItems, err := c.client.ListItems(ctx, 1, feedItemNum, "*")
+	if err != nil {
+		return err
+	}
+
+	atom, err := generateAtom(*qiitaItems)
+	if err != nil {
+		return err
+	}
+
+	return save(atom)
+}
+
+func generateAtom(qiitaItems qiita.Items) ([]byte, error) {
+	team := os.Getenv("QIITA_TEAM_NAME")
+
+	links := []atom.Link{
+		atom.Link{Href: "https://" + team + ".qiita.com"},
+	}
+
+	author := &atom.Person{
+		Name: team,
+	}
+
+	entries := []*atom.Entry{}
+
+	for _, item := range qiitaItems {
+		createdAt, err := stringToTime(item.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		updatedAt, err := stringToTime(item.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		entries = append(entries, &atom.Entry{
+			Title:     item.Title,
+			ID:        item.Id,
+			Link:      []atom.Link{atom.Link{Href: item.Url}},
+			Published: createdAt,
+			Updated:   updatedAt,
+			Author: &atom.Person{
+				Name: item.User.Id,
+				URI:  "https://" + team + ".qiita.com/" + item.User.Id + "/items",
+			},
+			Content: &atom.Text{
+				Type: "html",
+				Body: generateContent(*item.User),
+			},
+		})
+	}
+
+	feed := atom.Feed{
+		Title:   team + " Qiita:Team",
+		ID:      "https://" + team + ".qiita.com",
+		Link:    links,
+		Author:  author,
+		Updated: atom.Time(time.Now()),
+		Entry:   entries,
+	}
+
+	xmlBody, err := xml.Marshal(feed)
+	if err != nil {
+		return nil, err
+	}
+
+	return append([]byte(strings.TrimSpace(xml.Header)), xmlBody...), nil
+}
+
+func stringToTime(str string) (atom.TimeStr, error) {
+	t, err := time.Parse(time.RFC3339, str)
+	return atom.Time(t), err
+}
+
+func generateContent(user qiita.User) string {
+	m := map[string]interface{}{
+		"userID":      user.Id,
+		"userIconURL": user.ProfileImageUrl,
+	}
+	t := template.Must(template.New("").Parse(
+		`<a href="/{{.userID}}/items" rel="noreferrer">
+<img alt="@{{.userID}}" width="32" height="32" src="{{.userIconURL}}">
+</a>
+`))
+
+	var rendered bytes.Buffer
+	t.Execute(&rendered, m)
+
+	return rendered.String()
+}
+
+func save(content []byte) error {
+	db, err := NewDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return db.SetFeed(content)
+}
--- main.go
+++ main.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"context"
-	"encoding/xml"
 	"errors"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"strconv"
-	"strings"
-	"time"
-
-	"github.com/ktsujichan/qiita-sdk-go/qiita"
-	"golang.org/x/tools/blog/atom"
 )
 
 func main() {
@@ -92,120 +83,15 @@ func getFeed(user, token string) (string, error) {
 }
 
 func cli() error {
-	config := qiita.NewConfig()
-	config.WithEndpoint(fmt.Sprintf("https://%s.qiita.com", os.Getenv("QIITA_TEAM_NAME")))
-
-	c, err := qiita.NewClient(os.Getenv("QIITA_ACCESS_TOKEN"), *config)
+	c, err := NewCLI(os.Getenv("QIITA_TEAM_NAME"), os.Getenv("QIITA_ACCESS_TOKEN"))
 	if err != nil {
 		return err
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
 
-	perPage, err := strconv.Atoi(os.Getenv("FEED_ITEM_NUM"))
-	if err != nil || perPage < 1 {
+	feedItemNum, err := strconv.Atoi(os.Getenv("FEED_ITEM_NUM"))
+	if err != nil || feedItemNum < 1 {
 		return errors.New("$FEED_ITEM_NUM should be larger than zero")
 	}
 
-	qiitaItems, err := c.ListItems(ctx, 1, uint(perPage), "*")
-	if err != nil {
-		return err
-	}
-
-	atom, err := generateAtom(*qiitaItems)
-	if err != nil {
-		return err
-	}
-
-	return save(atom)
-}
-
-func generateAtom(qiitaItems qiita.Items) ([]byte, error) {
-	team := os.Getenv("QIITA_TEAM_NAME")
-
-	links := []atom.Link{
-		atom.Link{Href: "https://" + team + ".qiita.com"},
-	}
-
-	author := &atom.Person{
-		Name: team,
-	}
-
-	entries := []*atom.Entry{}
-
-	for _, item := range qiitaItems {
-		createdAt, err := stringToTime(item.CreatedAt)
-		if err != nil {
-			return nil, err
-		}
-
-		updatedAt, err := stringToTime(item.UpdatedAt)
-		if err != nil {
-			return nil, err
-		}
-
-		entries = append(entries, &atom.Entry{
-			Title:     item.Title,
-			ID:        item.Id,
-			Link:      []atom.Link{atom.Link{Href: item.Url}},
-			Published: createdAt,
-			Updated:   updatedAt,
-			Author: &atom.Person{
-				Name: item.User.Id,
-				URI:  "https://" + team + ".qiita.com/" + item.User.Id + "/items",
-			},
-			Content: &atom.Text{
-				Type: "html",
-				Body: generateContent(*item.User),
-			},
-		})
-	}
-
-	feed := atom.Feed{
-		Title:   team + " Qiita:Team",
-		ID:      "https://" + team + ".qiita.com",
-		Link:    links,
-		Author:  author,
-		Updated: atom.Time(time.Now()),
-		Entry:   entries,
-	}
-
-	xmlBody, err := xml.Marshal(feed)
-	if err != nil {
-		return nil, err
-	}
-
-	return append([]byte(strings.TrimSpace(xml.Header)), xmlBody...), nil
-}
-
-func stringToTime(str string) (atom.TimeStr, error) {
-	t, err := time.Parse(time.RFC3339, str)
-	return atom.Time(t), err
-}
-
-func generateContent(user qiita.User) string {
-	m := map[string]interface{}{
-		"userID":      user.Id,
-		"userIconURL": user.ProfileImageUrl,
-	}
-	t := template.Must(template.New("").Parse(
-		`<a href="/{{.userID}}/items" rel="noreferrer">
-<img alt="@{{.userID}}" width="32" height="32" src="{{.userIconURL}}">
-</a>
-`))
-
-	var rendered bytes.Buffer
-	t.Execute(&rendered, m)
-
-	return rendered.String()
-}
-
-func save(content []byte) error {
-	db, err := NewDB()
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	return db.SetFeed(content)
+	return c.Run(uint(feedItemNum))
 }
