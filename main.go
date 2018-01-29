bd4f2cd01434662463a0a4bc2b2b58e19ab3045c
Takashi Masuda <masutaka.net@gmail.com>
2018-01-29 23:54:49 +0900

    Fix required $FEED_ITEM_NUM

--- main.go
+++ main.go
@@ -17,12 +17,21 @@ func main() {
 			log.Fatal(err)
 		}
 
-		feedItemNum, err := strconv.Atoi(os.Getenv("FEED_ITEM_NUM"))
-		if err != nil || feedItemNum < 1 {
+		// The default of per_page is 20.
+		// See https://qiita.com/api/v2/docs
+		num := 20
+		if numStr := os.Getenv("FEED_ITEM_NUM"); numStr != "" {
+			num, err = strconv.Atoi(numStr)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if num < 1 {
 			log.Fatal(errors.New("$FEED_ITEM_NUM should be larger than zero"))
 		}
 
-		if err := c.Run(uint(feedItemNum)); err != nil {
+		if err := c.Run(uint(num)); err != nil {
 			log.Fatal(err)
 		}
 	} else {
